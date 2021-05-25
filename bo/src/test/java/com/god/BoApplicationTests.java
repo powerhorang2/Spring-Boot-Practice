171ae13dea261237562b5a0f078175f7a76660ea
powerhorang2 <tjrdbqls3@naver.com>
2021-05-25 15:22:14 +0900

    2021-05-25 / spring boot web 프로젝트 생성 / powerhorang2

---
+++ bo/src/test/java/com/god/BoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.god;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
