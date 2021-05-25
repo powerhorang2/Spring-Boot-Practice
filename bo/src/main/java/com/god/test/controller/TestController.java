defeb66da9af04e9dd7b61779bd3725d3b74fd8b
powerhorang2 <tjrdbqls3@naver.com>
2021-05-25 17:51:34 +0900

    [스프링부트 (3)] SpringMVC(2) Spring Boot View 설정 및 JSP 연동하기(Thymeleaf 추가)
    thymeleaf
    
    7. ▶ Dependency 추가
    	- pom.xml
    
    8. ▶ 파일 기본 경로 및 html 생성
    	- 경로 : /src/main/resources/templates/thymeleaf
    	- thymeleafTest.html
    
    9. ▶ application.properties 설정
    	- 컨트롤러가 뷰 이름을 반환할때 thymeleaf/로 시작하면 타임리프로 처리하도록 설정
    
    10. ▶ Vo 설정
    	- TestVo 생성
    
    11. ▶ Controller 설정
    	- /thymeleafTest 로 매핑되는 컨트롤러 method 생성
    
    출처: https://goddaehee.tistory.com/204?category=367461 [갓대희의 작은공간]
    
    2021-05-25 / powerhorang2

--- bo/src/main/java/com/god/test/controller/TestController.java
+++ bo/src/main/java/com/god/test/controller/TestController.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.god.test.vo.TestVo;
 
 @Controller
 public class TestController {
@@ -27,13 +30,20 @@ public class TestController {
 	@RequestMapping("/test")
 	public ModelAndView test() throws Exception {
 		ModelAndView mav = new ModelAndView("test");
-		mav.addObject("name", "goddaehee");
+		mav.addObject("name", "powerhorang2");
 		List<String> testList = new ArrayList<String>();
 		testList.add("a");
 		testList.add("b");
 		testList.add("c");
 		mav.addObject("list", testList);
 		return mav;
+	}
+
+	@RequestMapping("/thymeleafTest")
+	public String thymeleafTest(Model model) {
+		TestVo testModel = new TestVo("powerhorang2", "강한호랑이");
+		model.addAttribute("testModel", testModel);
+		return "thymeleaf/thymeleafTest";
 	}
 
 }
