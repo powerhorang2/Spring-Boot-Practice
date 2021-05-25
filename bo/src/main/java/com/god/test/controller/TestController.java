23dabd15e81546c8ac0d42fb8d05549d3c523adc
powerhorang2 <tjrdbqls3@naver.com>
2021-05-25 17:20:27 +0900

    [스프링부트 (3)] SpringMVC(2) Spring Boot View 설정 및 JSP 연동하기(Thymeleaf 추가)
    controller 설정
    
    3. ▶ controller에서 보내준 testList를 jsp의 c:forEach jstl 사용 가능 여부 확인을 위해서
    controller 수정
    
    출처: https://goddaehee.tistory.com/204?category=367461 [갓대희의 작은공간]
    
    2021-05-25 / powerhorang2

--- bo/src/main/java/com/god/test/controller/TestController.java
+++ bo/src/main/java/com/god/test/controller/TestController.java
@@ -1,22 +1,39 @@
 package com.god.test.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
-	
+
 	@RequestMapping(value = "/home")
 	public String home() {
-		
+
 		return "index.html";
 	}
-	
+
 	@ResponseBody
 	@RequestMapping("/valueTest")
 	public String valueTest() {
 		String value = "테스트 String";
 		return value;
 	}
+
+	@RequestMapping("/test")
+	public ModelAndView test() throws Exception {
+		ModelAndView mav = new ModelAndView("test");
+		mav.addObject("name", "goddaehee");
+		List<String> testList = new ArrayList<String>();
+		testList.add("a");
+		testList.add("b");
+		testList.add("c");
+		mav.addObject("list", testList);
+		return mav;
+	}
+
 }
