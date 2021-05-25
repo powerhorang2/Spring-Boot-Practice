8e627850f458efb26f327e972421787f543ace7f
powerhorang2 <tjrdbqls3@naver.com>
2021-05-25 16:07:35 +0900

    [스프링부트 (2)] SpringMVC(1) Controller 생성하기
    5. ▶ @RestController 생성
    
    2021-05-25 / powerhorang2
    
    출처: https://goddaehee.tistory.com/203?category=367461 [갓대희의 작은공간]

--- bo/src/main/java/com/god/test/controller/TestRestController.java
+++ bo/src/main/java/com/god/test/controller/TestRestController.java
@@ -1,5 +1,15 @@
 package com.god.test.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestRestController {
 
+	@RequestMapping(value="/testValue", method = RequestMethod.GET)
+	public String getTestValue() {
+		String testValue = "RestController 테스트";
+		return testValue;
+	}
 }
